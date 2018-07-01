707a2e47672e2f63329648fbbde37a5acebf4e07
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-01 11:21:38 -0700

    create dotfiles only when userHome is avaiable

--- install.sh
+++ install.sh
@@ -4,12 +4,14 @@ set -e
 echo "Installing nix profile"
 nix-env -i all
 
-# pushd $HOME > /dev/null
-#
-# echo "Creating dotfiles links in user home"
-# find .nix-profile/userHome/ -maxdepth 1 | sed "s/.nix-profile\/userHome\///g" | \
-#     grep -v "^$" | xargs -I {} ln -sf .nix-profile/userHome/{} {}
-#
-# popd > /dev/null
+if [[ -d "${HOME}/.nix-profile/userHome" ]]; then
+	pushd $HOME > /dev/null
+
+	echo "Creating dotfiles links in user home"
+	find .nix-profile/userHome/ -maxdepth 1 | sed "s/.nix-profile\/userHome\///g" | \
+			grep -v "^$" | xargs -I {} ln -sf .nix-profile/userHome/{} {}
+
+	popd > /dev/null
+fi
 
 echo "Done"
