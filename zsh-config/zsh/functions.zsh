2b9869d9b3061e2ff53f0d083cc64f447a8d948a
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-06 01:30:11 -0700

    oops too sleepy

--- zsh-config/zsh/functions.zsh
+++ zsh-config/zsh/functions.zsh
@@ -1,19 +1,19 @@
 # add the zsh functions to the fpath
-fpath+="@zsh_out@/zsh/functions"
-fpath+="@zsh_out@/zsh/completions"
+fpath+="@out_dir@/zsh/functions"
+fpath+="@out_dir@/zsh/completions"
 
 # load the important functions
-source "@zsh_out@/zsh/functions/debug"
-source "@zsh_out@/zsh/functions/have"
-source "@zsh_out@/zsh/functions/is_func"
-source "@zsh_out@/zsh/functions/pathappend"
-source "@zsh_out@/zsh/functions/pathprepend"
-source "@zsh_out@/zsh/functions/pathunmunge"
-source "@zsh_out@/zsh/functions/sp"
-source "@zsh_out@/zsh/functions/ssh_agents"
+source "@out_dir@/zsh/functions/debug"
+source "@out_dir@/zsh/functions/have"
+source "@out_dir@/zsh/functions/is_func"
+source "@out_dir@/zsh/functions/pathappend"
+source "@out_dir@/zsh/functions/pathprepend"
+source "@out_dir@/zsh/functions/pathunmunge"
+source "@out_dir@/zsh/functions/sp"
+source "@out_dir@/zsh/functions/ssh_agents"
 
 # autoload all of the functions
-for func in @zsh_out@/zsh/functions/*; do
+for func in @out_dir@/zsh/functions/*; do
   case "$(basename ${func})" in
     debug)       ;;
     have)        ;;
