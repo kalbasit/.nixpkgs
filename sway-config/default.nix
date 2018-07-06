1192429df670b3db06001ca31a32ad628d9efb86
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-06 12:50:05 -0700

    almost done, missing derivation name that provides pactl

--- sway-config/default.nix
+++ sway-config/default.nix
@@ -1,4 +1,4 @@
-{stdenv, brightnessctl, i3lock, rofi, termite, libnotify}:
+{stdenv, brightnessctl, i3lock, rofi, termite, libnotify, slack}:
 
 stdenv.mkDerivation rec {
   name = "sway-config";
@@ -8,13 +8,17 @@ stdenv.mkDerivation rec {
   src = ./.;
 
   installPhase = ''
+    cp $src/bin $out/bin
+
     install -d -m755 $out/userHome/.config/sway
     substitute $src/config $out/userHome/.config/sway/config \
+      --subst-var-by @out_dir@ $out \
       --subst-var-by @brightnessctl_bin@ ${brightnessctl}/bin/brightnessctl \
       --subst-var-by @pactl_bin@ ${TODO}/bin/pactl \
       --subst-var-by @i3lock_bin@ ${i3lock}/bin/i3lock \
       --subst-var-by @rofi_bin@ ${rofi}/bin/rofi \
       --subst-var-by @termite_bin@ ${termite}/bin/termite \
       --subst-var-by @notify-send_bin@ ${libnotify}/bin/notify-send \
+      --subst-var-by @slack_bin@ ${slack}/bin/slack
   '';
 }
