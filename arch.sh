9eeaa47c0ffdbd5f62fa487ed4def2bcae0dbca6
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-07 22:25:13 -0700

    split arch stuff to arch.sh

---
+++ arch.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# make sure pacman is colorful
+if grep -q '^#Color' /etc/pacman.conf; then
+	sudo sed -e 's:^#Color:Color:g' -i /etc/pacman.conf
+fi
+
+# NOTE: This is needed because Termite could not find the xterm-termite terminfo
+# See https://github.com/NixOS/nixpkgs/issues/19785
+if [[ ! -L .terminfo ]]; then
+	echo "Linking terminfo to allow terminals to find installed terminfo"
+	ln -sf .nix-profile/share/terminfo .terminfo
+fi
+
+while read line; do
+	if [[ "$(pacman -Qs "^${line}$" | wc -l)" -eq 0 ]]; then
+		echo "installing ${line} in the OS"
+		yay -S --nocleanmenu --nodiffmenu --noeditmenu --noupgrademenu "${line}"
+	fi
+done < <(grep -v '^#\|^$' ArchPackages)
+
+if [[ ! -f /etc/ca-certificates/trust-source/anchors/nasreddine.crt ]]; then
+	curl -LO http://nasreddine.com/ca.crt
+	sudo mv ca.crt /etc/ca-certificates/trust-source/anchors/nasreddine.crt
+	sudo trust extract-compat
+fi
